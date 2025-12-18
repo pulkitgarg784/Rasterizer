2a3def3a0713bf09d5522e9ae1ba521339b053f6
Pulkit Garg <pulkitgarg784@gmail.com>
2025-12-18 00:21:33 -0500

    Zbuffer

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,9 @@ constexpr TGAColor yellow = {0, 200, 255, 255};
 constexpr int width = 800;
 constexpr int height = 800;
 
-std::tuple<int, int> project(vec3 v) {
-  return {(v.x + 1.) * width / 2, (v.y + 1.) * height / 2};
+std::tuple<int, int, int> project(vec3 v) {
+  return {(v.x + 1.) * width / 2, (v.y + 1.) * height / 2,
+          (v.z + 1.) * 255.0 / 2};
 }
 
 void line(int ax, int ay, int bx, int by, TGAImage& framebuffer,
@@ -37,29 +38,34 @@ void line(int ax, int ay, int bx, int by, TGAImage& framebuffer,
   }
 }
 
-
-// Edge function: returns positive if (px,py) is on the left side of edge (ax,ay)->(bx,by)
-inline int edge(int ax, int ay, int bx, int by, int px, int py) {
-    return (px - ax) * (by - ay) - (py - ay) * (bx - ax);
+double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
+  return .5 * ((by - ay) * (bx + ax) + (cy - by) * (cx + bx) +
+               (ay - cy) * (ax + cx));
 }
 
-void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
-    int minx = std::min({ax, bx, cx});
-    int miny = std::min({ay, by, cy});
-    int maxx = std::max({ax, bx, cx});
-    int maxy = std::max({ay, by, cy});
-
+void triangle(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy,
+              int cz, TGAImage& zbuffer, TGAImage& framebuffer,
+              TGAColor color) {
+  int bbminx = std::min(std::min(ax, bx), cx);
+  int bbminy = std::min(std::min(ay, by), cy);
+  int bbmaxx = std::max(std::max(ax, bx), cx);
+  int bbmaxy = std::max(std::max(ay, by), cy);
+  double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
+  if (total_area < 1) return;
 #pragma omp parallel for
-    for (int y = miny; y <= maxy; y++) {
-        for (int x = minx; x <= maxx; x++) {
-            int w0 = edge(bx, by, cx, cy, x, y);
-            int w1 = edge(cx, cy, ax, ay, x, y);
-            int w2 = edge(ax, ay, bx, by, x, y);
-            // Point is inside if all edge functions have the same sign
-            if ((w0 >= 0 && w1 >= 0 && w2 >= 0) || (w0 <= 0 && w1 <= 0 && w2 <= 0))
-                framebuffer.set(x, y, color);
-        }
+  for (int x = bbminx; x <= bbmaxx; x++) {
+    for (int y = bbminy; y <= bbmaxy; y++) {
+      double alpha = signed_triangle_area(x, y, bx, by, cx, cy) / total_area;
+      double beta = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
+      double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
+      if (alpha < 0 || beta < 0 || gamma < 0) continue;
+      unsigned char z =
+          static_cast<unsigned char>(alpha * az + beta * bz + gamma * cz);
+      if (z <= zbuffer.get(x, y)[0]) continue;
+      zbuffer.set(x, y, {z});
+      framebuffer.set(x, y, color);
     }
+  }
 }
 
 int main(int argc, char** argv) {
@@ -71,19 +77,17 @@ int main(int argc, char** argv) {
   model.normalize();
 
   TGAImage framebuffer(width, height, TGAImage::RGB);
-
+  TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
   for (int i = 0; i < model.nfaces(); ++i) {
-    auto [ax, ay] = project(model.vertex(i, 0));
-    auto [bx, by] = project(model.vertex(i, 1));
-    auto [cx, cy] = project(model.vertex(i, 2));
-    // line(ax, ay, bx, by, framebuffer, white);
-    // line(bx, by, cx, cy, framebuffer, white);
-    // line(cx, cy, ax, ay, framebuffer, white);
+    auto [ax, ay, az] = project(model.vertex(i, 0));
+    auto [bx, by, bz] = project(model.vertex(i, 1));
+    auto [cx, cy, cz] = project(model.vertex(i, 2));
     TGAColor rnd;
     for (int c = 0; c < 3; c++) rnd[c] = std::rand() % 255;
-    triangle(ax, ay, bx, by, cx, cy, framebuffer, rnd);
+    triangle(ax, ay, az, bx, by, bz, cx, cy, cz, zbuffer, framebuffer, rnd);
   }
 
   framebuffer.write_tga_file("framebuffer.tga");
+  zbuffer.write_tga_file("zbuffer.tga");
   return 0;
 }
